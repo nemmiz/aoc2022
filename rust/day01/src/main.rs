8aaf8b5a51a205f2205295b0de615aaa508d6c7c
Emma Norgren <emma@DESKTOP-VED8IIO.localdomain>
2022-12-01 22:27:10 +0100

    Day 1

---
+++ rust/day01/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let input = include_str!("../../../input/01.txt");
+
+    let mut calories = vec![0];
+    for line in input.lines() {
+        if line.trim().is_empty() {
+            calories.push(0);
+        } else {
+            let n: i32 = line.parse::<i32>().expect("Not a number: {line}");
+            *calories.last_mut().unwrap() += n;
+        }
+    }
+
+    let max = calories.iter().max().unwrap();
+    println!("{max}");
+
+    calories.sort();
+    let len = calories.len();
+    let last3 = &calories[len - 3..];
+    let sumlast3: i32 = last3.iter().sum();
+    println!("{sumlast3}");
+}
